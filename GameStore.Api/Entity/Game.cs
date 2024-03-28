21e7d804a8eb69a4d1f589dc5890008f5a7e492d
Yeabsra <tesfayeyeabsra446@gmail.com>
2024-03-28 11:36:28 +0300

    code refactored to and created GamesEndpint

--- GameStore.Api/Entity/Game.cs
+++ GameStore.Api/Entity/Game.cs
@@ -1,18 +1,25 @@
 
 
-namespace GameStore.Api.Entity
-{
+using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.Api.Entity;
+
     public class Game
     {
 
      public int Id {get; set;} 
+     [Required]
+     [StringLength(50)]
      public required string Name {get; set;}
-
+      [Required]
+     [StringLength(20)]
      public required string Genere { get; set; }
-
+      [Required]
+       [Range(1,100)]
      public decimal Price  { get; set; }
      public DateTime RealseDate { get; set; }
-
+     
+     [Url]
+     [StringLength(100)]
      public required string ImageUrl { get; set; }
     }
-}
--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9880cdaf4783d15a9a5d2fac052fe87a92798d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aec296607c0f3cd275895462b28755f79ffb910f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
